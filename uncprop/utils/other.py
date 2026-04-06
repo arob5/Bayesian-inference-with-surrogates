fadbf66c3ba1c820d264b0bc08bb9c3c36a927b0
Andrew Roberts <agr.roberts@gmail.com>
2026-04-06 11:43:03 -0400

    fix JAX int overflow in _numpy_rng_seed_from_jax_key: use 2**31 not 2**63

--- uncprop/utils/other.py
+++ uncprop/utils/other.py
@@ -5,4 +5,4 @@ from uncprop.custom_types import PRNGKey
 
 
 def _numpy_rng_seed_from_jax_key(key: PRNGKey) -> int:
-    return int(jr.randint(key, (), 0, 2**63 - 1))
+    return int(jr.randint(key, (), 0, 2**31 - 1))
